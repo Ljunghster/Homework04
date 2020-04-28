94223d72ebb09050811490ac182e7faa5d07d37b
Ljunghster <mpljungh@gmail.com>
2020-04-27 20:42:09 -0700

    set up score counter, but need to make wrong answers allow user to move on to next question rather than simply subtract from the timer

--- quiz.js
+++ quiz.js
@@ -1,5 +1,8 @@
 var timeRemainingSec = 75;
 
+var score = 0;
+document.getElementById('score').innerHTML = score
+
 document.getElementById('start').onclick = function () {
 
     document.getElementById('timerSec').innerHTML = timeRemainingSec;
@@ -16,35 +19,52 @@ document.getElementById('start').onclick = function () {
 document.getElementById('answer1').onclick = function () {
     document.getElementById('quizQ1').style.display = 'none'
     document.getElementById('quizQ2').style.display = 'block'
+    updateScore()
 };
 
 document.getElementById('answer2').onclick = function () {
     document.getElementById('quizQ2').style.display = 'none'
     document.getElementById('quizQ3').style.display = 'block'
+    updateScore()
 };
 
 document.getElementById('answer3').onclick = function () {
     document.getElementById('quizQ3').style.display = 'none'
     document.getElementById('quizQ4').style.display = 'block'
+    updateScore()
 };
 
 document.getElementById('answer4').onclick = function () {
     document.getElementById('quizQ4').style.display = 'none'
     document.getElementById('quizQ5').style.display = 'block'
+    updateScore()
 };
 
 document.getElementById('answer5').onclick = function () {
     document.getElementById('quizQ5').style.display = 'none'
     document.getElementById('unicornEgg').style.display = 'block'
+    updateScore()
 };
 
 var wrongAnswers = document.getElementsByClassName('wrong');
 var i;
-
 for (i = 0; i < wrongAnswers.length; i++) {
-    answer = wrongAnswers[i]
+    var answer = wrongAnswers[i]
     answer.onclick = function () {
         timeRemainingSec = timeRemainingSec - 5;
         document.getElementById('timerSec').innerHTML = timeRemainingSec;
     }
 }
+
+var updateScore = function ()    {
+    score = score + 76.5;
+    document.getElementById('scoreBox').innerHTML = score;
+}
+
+// var correctAnswers = document.getElementsByClassName('correct');
+// for (i = 0; i < correctAnswers.length; i++) {
+//     answer = correctAnswers[i]
+//     answer.setAttribute('onclick', answer.getAttribute('onclick') + '; updateScore();')
+// }
+
+
